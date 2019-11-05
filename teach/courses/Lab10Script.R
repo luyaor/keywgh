93be3627ff478d52f171b981e5ca9f3c1d981a03
Ganghua Wang <keywgh@gmail.com>
2019-11-04 19:15:44 -0600

    Update Rcode

--- teach/courses/Lab10Script.R
+++ teach/courses/Lab10Script.R
@@ -6,15 +6,24 @@
 #ifelse checks whether test.statistic is negative or positive.  If negative, it calculates p-value from left tail, otherwise it uses the right tail.
 
 
-set.seed(231)
+set.seed(231)  # To reproduce results
+alpha = 0.05  # Significant level 
+reject = 0  # Times of rejecting H_0
 
 for (i in 1:20) {
 	samp<-rnorm(25,mean=475,sd=10)
-	test.statistic<-(mean(samp)-475)/(sd(samp)/sqrt(25))
-	ifelse(test.statistic <0, p_value<-2*pt(test.statistic,df=24),  
-	       p_value<-2*pt(test.statistic,df=24,lower=F))
+	# Use t.test to obtain p.value
+	p_value = t.test(samp, mu=475, conf.level=1-alpha)$p.value
+	
+  # Manually calculate p value
+	# test.statistic<-(mean(samp)-475)/(sd(samp)/sqrt(25))
+	# p_value<-2*pt(abs(test.statistic),df=24,lower=F)
+	
 	cat("Day",i,":","P-value=",round(p_value,3),"\n")
+	if (p_value <= alpha){reject = reject+1}  # Count the rejection
 	}
+
+(reject)
 
 #######################################################
 #Cruella de Vil's Data - 1 day
@@ -22,7 +31,11 @@ for (i in 1:20) {
 #Part 2
 glucosamine<-c(464,470,467,478,474,480,472,485,480,470,472,479,477,480,478,482,
                488,489,480,475,483,488,478,490,492)
-
+qqnorm(glucosamine)
+qqline(glucosamine)
+test.statistic<-(mean(glucosamine)-475)/(sd(glucosamine)/sqrt(25))
+p_value<-2*pt(abs(test.statistic),df=24,lower=F)
+p_value = t.test(glucosamine, mu=475, conf.level=1-alpha)$p.value
 
 #######################################################
 #Cruella de Vil's Data -  20 days
@@ -34,15 +47,19 @@ glucosamine<-c(464,470,467,478,474,480,472,485,480,470,472,479,477,480,478,482,
 #and sd=10
 
 set.seed(51)
+reject = 0  # Times of rejecting H_0
 
 for (i in 1:20) {
 	samp<-rnorm(25,mean=477.5,sd=10)
-	test.statistic<-(mean(samp)-475)/(sd(samp)/sqrt(25))
-	ifelse(test.statistic <0, p_value<-2*pt(test.statistic,df=24),  
-	       p_value<-2*pt(test.statistic,df=24,lower=F))
-	LCL<-mean(samp) - qt(0.025,df=24,lower=F)*(sd(samp)/sqrt(25))
-	UCL<-mean(samp) + qt(0.025,df=24,lower=F)*(sd(samp)/sqrt(25))
-cat("Day",i,":","P-value=",round(p_value,3),";","95% CI=(",round(LCL,2),",",
-    round(UCL,2),").\n")
+	p_value = t.test(samp, mu=475, conf.level=1-alpha)$p.value
+	ci = t.test(samp, mu=475, conf.level=1-alpha)$conf.int
+	
+	# manually calculate lower and upper bound of CI
+	# LCL<-mean(samp) - qt(0.025,df=24,lower=F)*(sd(samp)/sqrt(25))
+	# UCL<-mean(samp) + qt(0.025,df=24,lower=F)*(sd(samp)/sqrt(25))
+  cat("Day",i,":","P-value=",round(p_value,3),";","95% CI=(",round(ci,2),").\n")
+  if (p_value <= alpha){reject = reject+1}  # Count the rejection
 }
+
+(reject)
 
